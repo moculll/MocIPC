930990ccdbd04a7b644ee8d1727d63d31628caa8
mocul <823309460@qq.com>
2024-08-31 19:24:28 +0800

    add UNICODE & UTF-8 compile support

--- mocIPC.cpp
+++ mocIPC.cpp
@@ -5,180 +5,197 @@
 
 #define MOCIPC_DBGPRINT_ENABLE 1
 #if MOCIPC_DBGPRINT_ENABLE
+
 #define MOCIPC_DBGPRINT(fmt, ...) \
             do { \
                 printf(fmt " [%s]" "\n", ##__VA_ARGS__, __func__); \
             } while(0)
+
 #else
 #define MOCIPC_DBGPRINT(fmt, ...)
 #endif
 
 namespace MocIPC {
-const wchar_t* IPCBase::MOCIPC_DEFAULT_SHAREDSVC = L"\\\\.\\pipe\\MOCSVC";
-const wchar_t* IPCBase::MOCIPC_DEFAULT_SHAREDCVS = L"\\\\.\\pipe\\MOCCVS";
+#if UNICODE
+    const CHARTYPE* IPCBase::MOCIPC_DEFAULT_SHAREDSVC = L"\\\\.\\pipe\\MOCSVC";
+    const CHARTYPE* IPCBase::MOCIPC_DEFAULT_SHAREDCVS = L"\\\\.\\pipe\\MOCCVS";
+#else
+    const CHARTYPE* IPCBase::MOCIPC_DEFAULT_SHAREDSVC = "\\\\.\\pipe\\MOCSVC";
+    const CHARTYPE* IPCBase::MOCIPC_DEFAULT_SHAREDCVS = "\\\\.\\pipe\\MOCCVS";
+#endif
 
-IPCBase::IPCBase(const wchar_t* _sharedSVCName, const wchar_t* _sharedCVSName)
-{
-    size_t svcSize = wcslen(_sharedSVCName) + 1;
-    size_t cvsSize = wcslen(_sharedCVSName) + 1;
-    this->recvHOOK = nullptr;
-    try {
-        this->sharedSVCName = new wchar_t[svcSize];
-        this->sharedCVSName = new wchar_t[cvsSize];
-        this->buffer = new char[1024];
+    IPCBase::IPCBase(const CHARTYPE* _sharedSVCName, const CHARTYPE* _sharedCVSName)
+    {
+#if UNICODE
+        size_t svcSize = wcslen(_sharedSVCName) + 1;
+        size_t cvsSize = wcslen(_sharedCVSName) + 1;
+#else
+        size_t svcSize = strlen(_sharedSVCName) + 1;
+        size_t cvsSize = strlen(_sharedCVSName) + 1;
+#endif
+
+        this->recvHOOK = nullptr;
+        try {
+            this->sharedSVCName = new CHARTYPE[svcSize];
+            this->sharedCVSName = new CHARTYPE[cvsSize];
+            this->buffer = new char[1024];
+        }
+        catch (const std::bad_alloc& e) {
+            MOCIPC_DBGPRINT("Memory allocation failed: %s, check your memory!", e.what());
+            this->sharedSVCName = nullptr;
+            this->sharedCVSName = nullptr;
+            this->buffer = nullptr;
+            return;
+        }
+#if UNICODE
+        wcscpy_s(this->sharedSVCName, svcSize, _sharedSVCName);
+        wcscpy_s(this->sharedCVSName, cvsSize, _sharedCVSName);
+#else
+        strcpy_s(this->sharedSVCName, svcSize, _sharedSVCName);
+        strcpy_s(this->sharedCVSName, cvsSize, _sharedCVSName);
+#endif
+        memset((void*)this->buffer, 0, 1024);
+
+        MOCIPC_DBGPRINT("create Instance successful!");
+
     }
-    catch (const std::bad_alloc& e) {
-        MOCIPC_DBGPRINT("Memory allocation failed: %s, check your memory!", e.what());
-        this->sharedSVCName = nullptr;
-        this->sharedCVSName = nullptr;
-        this->buffer = nullptr;
-        return;
+
+    HANDLE IPCBase::createNewPipe(const CHARTYPE* pipeName)
+    {
+        return CreateNamedPipe(
+            pipeName,
+            PIPE_ACCESS_DUPLEX,
+            PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
+            PIPE_UNLIMITED_INSTANCES,
+            1024,
+            1024,
+            NMPWAIT_USE_DEFAULT_WAIT,
+            NULL
+        );
     }
-    
-    wcscpy_s(this->sharedSVCName, svcSize, _sharedSVCName);
-    wcscpy_s(this->sharedCVSName, cvsSize, _sharedCVSName);
-    memset((void*)this->buffer, 0, 1024);
-
-    MOCIPC_DBGPRINT("create Instance successful!");
-
-}
-
-HANDLE IPCBase::createNewPipe(const wchar_t* pipeName)
-{
-    return CreateNamedPipe(
-        pipeName,
-        PIPE_ACCESS_DUPLEX,
-        PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
-        PIPE_UNLIMITED_INSTANCES,
-        1024,
-        1024,
-        NMPWAIT_USE_DEFAULT_WAIT,
-        NULL
-    );
-}
 
 } /* MocIPC IPCBase */
 
 
 namespace MocIPC {
 
-IPCServer::IPCServer(const wchar_t* sharedSVCName, const wchar_t* sharedCVSName) : IPCBase(sharedSVCName, sharedCVSName)
-{
-    this->recvThread = std::thread(&IPCServer::recvThreadCallback, this);
-}
-void IPCServer::recvThreadCallback(IPCServer* obj)
-{
-    HANDLE hPipe;
-    DWORD bytesRead;
-    while (true) {
-        WaitNamedPipe(obj->sharedCVSName, NMPWAIT_WAIT_FOREVER);
-        hPipe = CreateFile(
-            obj->sharedCVSName,
-            GENERIC_READ | GENERIC_WRITE,
-            FILE_SHARE_READ,
-            NULL,
-            OPEN_EXISTING,
-            0,
-            NULL
-        );
-        if (ReadFile(hPipe, obj->buffer + sizeof(uint32_t), 1024 - sizeof(uint32_t), &bytesRead, NULL)) {
-            if (obj->recvHOOK) {
-                *(uint32_t*)obj->buffer = bytesRead;
-                obj->recvHOOK((void*)obj->buffer);
+    IPCServer::IPCServer(const CHARTYPE* sharedSVCName, const CHARTYPE* sharedCVSName) : IPCBase(sharedSVCName, sharedCVSName)
+    {
+        this->recvThread = std::thread(&IPCServer::recvThreadCallback, this);
+    }
+    void IPCServer::recvThreadCallback(IPCServer* obj)
+    {
+        HANDLE hPipe;
+        DWORD bytesRead;
+        while (true) {
+            WaitNamedPipe(obj->sharedCVSName, NMPWAIT_WAIT_FOREVER);
+            hPipe = CreateFile(
+                obj->sharedCVSName,
+                GENERIC_READ | GENERIC_WRITE,
+                FILE_SHARE_READ,
+                NULL,
+                OPEN_EXISTING,
+                0,
+                NULL
+            );
+            if (ReadFile(hPipe, obj->buffer + sizeof(uint32_t), 1024 - sizeof(uint32_t), &bytesRead, NULL)) {
+                if (obj->recvHOOK) {
+                    *(uint32_t*)obj->buffer = bytesRead;
+                    obj->recvHOOK((void*)obj->buffer);
+                }
+
+                MOCIPC_DBGPRINT("receive len: %d", bytesRead);
             }
-                
-            MOCIPC_DBGPRINT("receive len: %d", bytesRead);
+
+            FlushFileBuffers(hPipe);
         }
 
-        FlushFileBuffers(hPipe);
     }
 
-}
-
-void IPCServer::write(void* src, uint32_t size)
-{
-    std::async(std::launch::async, [this, src, size]{ this->writeImpl(src, size); } );
-}
-
-void IPCServer::writeImpl(void* src, uint32_t size)
-{
-    DWORD bytesWritten;
-    HANDLE hPipe = createNewPipe(this->sharedSVCName);
-    if (hPipe == INVALID_HANDLE_VALUE) {
-        MOCIPC_DBGPRINT("Failed to connect to pipe. Error: %d", GetLastError());
-        return;
+    void IPCServer::write(void* src, uint32_t size)
+    {
+        std::async(std::launch::async, [this, src, size] { this->writeImpl(src, size); });
     }
-    ConnectNamedPipe(hPipe, NULL);
 
-    memcpy((void*)buffer, src, size);
-    if (!WriteFile(hPipe, (void*)buffer, size, &bytesWritten, NULL)) {
-        MOCIPC_DBGPRINT("Failed to write to pipe. Error: %d", GetLastError());
+    void IPCServer::writeImpl(void* src, uint32_t size)
+    {
+        DWORD bytesWritten;
+        HANDLE hPipe = createNewPipe(this->sharedSVCName);
+        if (hPipe == INVALID_HANDLE_VALUE) {
+            MOCIPC_DBGPRINT("Failed to connect to pipe. Error: %d", GetLastError());
+            return;
+        }
+        ConnectNamedPipe(hPipe, NULL);
+
+        memcpy((void*)buffer, src, size);
+        if (!WriteFile(hPipe, (void*)buffer, size, &bytesWritten, NULL)) {
+            MOCIPC_DBGPRINT("Failed to write to pipe. Error: %d", GetLastError());
+        }
+        DisconnectNamedPipe(hPipe);
+        MOCIPC_DBGPRINT("write buf len: %d", bytesWritten);
     }
-    DisconnectNamedPipe(hPipe);
-    MOCIPC_DBGPRINT("write buf len: %d", bytesWritten);
-}
 
 } /* MocIPC server */
 
 
 namespace MocIPC {
 
-IPCClient::IPCClient(const wchar_t* sharedSVCName, const wchar_t* sharedCVSName) : IPCBase(sharedSVCName, sharedCVSName)
-{
-    this->recvThread = std::thread(&IPCClient::recvThreadCallback, this);
-}
+    IPCClient::IPCClient(const CHARTYPE* sharedSVCName, const CHARTYPE* sharedCVSName) : IPCBase(sharedSVCName, sharedCVSName)
+    {
+        this->recvThread = std::thread(&IPCClient::recvThreadCallback, this);
+    }
 
-void IPCClient::recvThreadCallback(IPCClient* obj)
-{
-    HANDLE hPipe;
-    MOCIPC_DBGPRINT("done statrListening");
-    DWORD bytesRead;
-    while (true) {
-        WaitNamedPipe(obj->sharedSVCName, NMPWAIT_WAIT_FOREVER);
-        hPipe = CreateFile(
-            obj->sharedSVCName,
-            GENERIC_READ | GENERIC_WRITE,
-            FILE_SHARE_READ,
-            NULL,
-            OPEN_EXISTING,
-            0,
-            NULL
-        );
-        if (ReadFile(hPipe, obj->buffer + sizeof(uint32_t), 1024 - sizeof(uint32_t), &bytesRead, NULL)) {
-            if (obj->recvHOOK) {
-                *(uint32_t*)obj->buffer = bytesRead;
-                obj->recvHOOK((void*)obj->buffer);
+    void IPCClient::recvThreadCallback(IPCClient* obj)
+    {
+        HANDLE hPipe;
+        MOCIPC_DBGPRINT("done statrListening");
+        DWORD bytesRead;
+        while (true) {
+            WaitNamedPipe(obj->sharedSVCName, NMPWAIT_WAIT_FOREVER);
+            hPipe = CreateFile(
+                obj->sharedSVCName,
+                GENERIC_READ | GENERIC_WRITE,
+                FILE_SHARE_READ,
+                NULL,
+                OPEN_EXISTING,
+                0,
+                NULL
+            );
+            if (ReadFile(hPipe, obj->buffer + sizeof(uint32_t), 1024 - sizeof(uint32_t), &bytesRead, NULL)) {
+                if (obj->recvHOOK) {
+                    *(uint32_t*)obj->buffer = bytesRead;
+                    obj->recvHOOK((void*)obj->buffer);
+                }
+                MOCIPC_DBGPRINT("receive len: %d", bytesRead);
             }
-            MOCIPC_DBGPRINT("receive len: %d", bytesRead);
+
+            FlushFileBuffers(hPipe);
         }
 
-        FlushFileBuffers(hPipe);
     }
 
-}
-
-void IPCClient::write(void* src, uint32_t size)
-{
-    std::async(std::launch::async, [this, src, size] {this->writeImpl(src, size); });
-}
-
-void IPCClient::writeImpl(void* src, uint32_t size)
-{
-    DWORD bytesWritten;
-    HANDLE hPipe = createNewPipe(this->sharedCVSName);
-    if (hPipe == INVALID_HANDLE_VALUE) {
-        MOCIPC_DBGPRINT("Failed to connect to pipe. Error: %d", GetLastError());
-        return;
+    void IPCClient::write(void* src, uint32_t size)
+    {
+        std::async(std::launch::async, [this, src, size] {this->writeImpl(src, size); });
     }
-    ConnectNamedPipe(hPipe, NULL);
 
-    memcpy((void*)buffer, src, size);
-    if (!WriteFile(hPipe, (void*)buffer, size, &bytesWritten, NULL)) {
-        MOCIPC_DBGPRINT("Failed to write to pipe. Error: %d", GetLastError());
+    void IPCClient::writeImpl(void* src, uint32_t size)
+    {
+        DWORD bytesWritten;
+        HANDLE hPipe = createNewPipe(this->sharedCVSName);
+        if (hPipe == INVALID_HANDLE_VALUE) {
+            MOCIPC_DBGPRINT("Failed to connect to pipe. Error: %d", GetLastError());
+            return;
+        }
+        ConnectNamedPipe(hPipe, NULL);
+
+        memcpy((void*)buffer, src, size);
+        if (!WriteFile(hPipe, (void*)buffer, size, &bytesWritten, NULL)) {
+            MOCIPC_DBGPRINT("Failed to write to pipe. Error: %d", GetLastError());
+        }
+        DisconnectNamedPipe(hPipe);
+        MOCIPC_DBGPRINT("write buf len: %d", bytesWritten);
     }
-    DisconnectNamedPipe(hPipe);
-    MOCIPC_DBGPRINT("write buf len: %d", bytesWritten);
-}
 
 } /* MocIPC client */
 
--- mocIPC.h
+++ mocIPC.h
@@ -2,79 +2,85 @@
 #include <windows.h>
 #include <thread>
 
+#if UNICODE
+#define CHARTYPE wchar_t
+#else
+#define CHARTYPE char
+
+#endif
 namespace MocIPC {
 
-template <typename T>
-inline typename std::enable_if<std::is_pointer<T>::value, T>::type getArg(void* arg) {
-	if (!arg) return nullptr;
-	return reinterpret_cast<T>(reinterpret_cast<char*>(arg) + sizeof(uint32_t));
-}
-
-template <typename T>
-inline typename std::enable_if<!std::is_pointer<T>::value, T>::type getArg(void* arg) {
-	if (!arg) T();
-	return *reinterpret_cast<T*>(reinterpret_cast<char*>(arg) + sizeof(uint32_t));
-}
-
-inline uint32_t getSize(void *arg)
-{
-	return ( arg ? *(uint32_t*)arg : 0 );
-}
-
-class IPCBase {
-public:
-	using recvHookCallbackType = void(*)(void*);
-	static const wchar_t* MOCIPC_DEFAULT_SHAREDSVC;
-	static const wchar_t* MOCIPC_DEFAULT_SHAREDCVS;
-
-	virtual void write(void* src, uint32_t size) = 0;
-	IPCBase(const wchar_t* _sharedSVCName, const wchar_t* _sharedCVSName);
-	IPCBase() : IPCBase(MOCIPC_DEFAULT_SHAREDSVC, MOCIPC_DEFAULT_SHAREDCVS) {}
-	~IPCBase()
-	{
-		delete buffer;
-		delete sharedSVCName;
-		delete sharedCVSName;
-		buffer = nullptr;
-		sharedSVCName = nullptr;
-		sharedCVSName = nullptr;
-		recvHOOK = nullptr;
+	template <typename T>
+	inline typename std::enable_if<std::is_pointer<T>::value, T>::type getArg(void* arg) {
+		if (!arg) return nullptr;
+		return reinterpret_cast<T>(reinterpret_cast<char*>(arg) + sizeof(uint32_t));
 	}
-	void registerRecvHOOK(recvHookCallbackType cb)
-	{
-		recvHOOK = cb;
+
+	template <typename T>
+	inline typename std::enable_if<!std::is_pointer<T>::value, T>::type getArg(void* arg) {
+		if (!arg) T();
+		return *reinterpret_cast<T*>(reinterpret_cast<char*>(arg) + sizeof(uint32_t));
 	}
-	HANDLE createNewPipe(const wchar_t* pipeName);
 
-protected:
-	std::thread recvThread;
-	recvHookCallbackType recvHOOK;
-	wchar_t *sharedSVCName;
-	wchar_t *sharedCVSName;
-	char* buffer;
+	inline uint32_t getSize(void* arg)
+	{
+		return (arg ? *(uint32_t*)arg : 0);
+	}
 
-};
+	class IPCBase {
+	public:
+		using recvHookCallbackType = void(*)(void*);
+		static const CHARTYPE* MOCIPC_DEFAULT_SHAREDSVC;
+		static const CHARTYPE* MOCIPC_DEFAULT_SHAREDCVS;
 
-class IPCServer : public IPCBase {
-public:
-	IPCServer(const wchar_t* sharedSVCName, const wchar_t* sharedCVSName);
-	IPCServer() : IPCServer(MOCIPC_DEFAULT_SHAREDSVC, MOCIPC_DEFAULT_SHAREDCVS) {}
-	void write(void* src, uint32_t size) override;
-	
-private:
-	static void recvThreadCallback(IPCServer* obj);
-	void writeImpl(void* src, uint32_t size);
-};
+		virtual void write(void* src, uint32_t size) = 0;
+		IPCBase(const CHARTYPE* _sharedSVCName, const CHARTYPE* _sharedCVSName);
+		IPCBase() : IPCBase(MOCIPC_DEFAULT_SHAREDSVC, MOCIPC_DEFAULT_SHAREDCVS) {}
+		~IPCBase()
+		{
+			delete buffer;
+			delete sharedSVCName;
+			delete sharedCVSName;
+			buffer = nullptr;
+			sharedSVCName = nullptr;
+			sharedCVSName = nullptr;
+			recvHOOK = nullptr;
+		}
+		void registerRecvHOOK(recvHookCallbackType cb)
+		{
+			recvHOOK = cb;
+		}
+		HANDLE createNewPipe(const CHARTYPE* pipeName);
 
-class IPCClient : public IPCBase {
-public:
-	IPCClient(const wchar_t* sharedSVCName, const wchar_t* sharedCVSName);
-	IPCClient() : IPCClient(MOCIPC_DEFAULT_SHAREDSVC, MOCIPC_DEFAULT_SHAREDCVS) {}
-	void write(void* src, uint32_t size) override;
+	protected:
+		std::thread recvThread;
+		recvHookCallbackType recvHOOK;
+		CHARTYPE* sharedSVCName;
+		CHARTYPE* sharedCVSName;
+		char* buffer;
 
-private:
-	static void recvThreadCallback(IPCClient* obj);
-	void writeImpl(void* src, uint32_t size);
-};
+	};
+
+	class IPCServer : public IPCBase {
+	public:
+		IPCServer(const CHARTYPE* sharedSVCName, const CHARTYPE* sharedCVSName);
+		IPCServer() : IPCServer(MOCIPC_DEFAULT_SHAREDSVC, MOCIPC_DEFAULT_SHAREDCVS) {}
+		void write(void* src, uint32_t size) override;
+
+	private:
+		static void recvThreadCallback(IPCServer* obj);
+		void writeImpl(void* src, uint32_t size);
+	};
+
+	class IPCClient : public IPCBase {
+	public:
+		IPCClient(const CHARTYPE* sharedSVCName, const CHARTYPE* sharedCVSName);
+		IPCClient() : IPCClient(MOCIPC_DEFAULT_SHAREDSVC, MOCIPC_DEFAULT_SHAREDCVS) {}
+		void write(void* src, uint32_t size) override;
+
+	private:
+		static void recvThreadCallback(IPCClient* obj);
+		void writeImpl(void* src, uint32_t size);
+	};
 
 } /* MocIPC */
