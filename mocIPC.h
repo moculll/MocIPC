2867e1f7712d0fa6092aa74e5fdcc9c28943dee9
mocul <823309460@qq.com>
2024-09-10 20:32:59 +0800

    updating and this is a backup

--- mocIPC.h
+++ mocIPC.h
@@ -4,19 +4,20 @@
 #include <map>
 #include <vector>
 #include <string>
+#include <random>
 
-#if UNICODE
-#define CHARTYPE wchar_t
-#define STRINGLEN wcslen
-#define STRCPYSAFE wcscpy_s
-#define STRSTR wcsstr
+
+#define MOCIPC_DBGPRINT_ENABLE 1
+#if MOCIPC_DBGPRINT_ENABLE
+
+#define MOCIPC_DBGPRINT(fmt, ...) \
+            do { \
+                printf(fmt " [%s]" "\n", ##__VA_ARGS__, __func__); \
+            } while(0)
+
 #else
-#define CHARTYPE char
-#define STRINGLEN strlen
-#define STRCPYSAFE strcpy_s
-#define STRSTR strstr
+#define MOCIPC_DBGPRINT(fmt, ...)
 #endif
-
 namespace MocIPC {
 
 template <typename T>
@@ -36,20 +37,65 @@ inline uint32_t getSize(void* arg)
 	return (arg ? *(uint32_t*)arg : 0);
 }
 
+namespace IPCDefines {
+#if UNICODE
+using miCharType_t = wchar_t;
+using miStdString = std::wstring;
+#define miStrlen wcslen
+#define miStrcpy wcscpy_s
+#define miStrstr wcsstr
+#define miConstString(x) L##x
+#else
+using miCharType_t = char;
+using miStdString = std::string;
+#define miStrlen strlen
+#define miStrcpy strcpy_s
+#define miStrstr strstr
+#define miConstString(x) x
+#endif
 
+}
 
 namespace IPCStaticLibrary {
 
+template <typename T>
+std::size_t getExactSizeofString(const T& str) {
+	if constexpr (std::is_same_v<T, std::string>) {
+		return str.size() + 1;
+	}
+	else if constexpr (std::is_same_v<T, std::wstring>) {
+		return (str.size() + 1) * sizeof(wchar_t);
+	}
+	else {
+		static_assert(std::is_same_v<T, std::string> || std::is_same_v<T, std::wstring>,
+			"Unsupported string type");
+		return 0;
+	}
+}
+
+IPCDefines::miStdString generateRandomStdString(int length) {
+	IPCDefines::miStdString charset = miConstString("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+	IPCDefines::miStdString result;
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dis(0, charset.size() - 1);
+
+	for (int i = 0; i < length; ++i) {
+		result += charset[dis(gen)];
+	}
+
+	return result;
+}
 	
-static inline HANDLE createDefaultPipe(const CHARTYPE* pipeName)
+static inline HANDLE createDefaultPipe(const IPCDefines::miStdString &pipeName)
 {
 	return IPCStaticLibrary::createPipe(pipeName, 2048, 2048);
 }
 
-static inline HANDLE createPipe(const CHARTYPE* pipeName, uint32_t outBufferSize, uint32_t inBufferSize)
+static inline HANDLE createPipe(const IPCDefines::miStdString &pipeName, uint32_t outBufferSize, uint32_t inBufferSize)
 {
 	return CreateNamedPipe(
-		pipeName,
+		pipeName.c_str(),
 		PIPE_ACCESS_DUPLEX,
 		PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
 		PIPE_UNLIMITED_INSTANCES,
@@ -69,23 +115,22 @@ public:
 		
 	}
 
-	HANDLE openChannel(const CHARTYPE* pipeName)
+	HANDLE openChannel(const IPCDefines::miStdString &pipeName)
 	{
 		IPCStaticLibrary::createDefaultPipe(pipeName);
-		return 
+		return ;
 	}
 	void closeChannel(HANDLE handleToClose)
 	{
 
 	}
 protected:
-	std::map<HANDLE, CHARTYPE*> 
-	HANDLE 
+	/*std::map<HANDLE, CHARTYPE*> a;*/
+
 
 
 private:
 	
-	std::thread
 
 
 	
@@ -95,19 +140,78 @@ private:
 
 class IPCServer : public IPCUnit {
 public:
+	
+	
 
+	IPCServer() 
+	{
+		connMgr = std::thread([this] {
+			HANDLE daemonHandle = INVALID_HANDLE_VALUE;
+			HANDLE newConnectionHandle = INVALID_HANDLE_VALUE;
+			DWORD bytesWritten = 0;
+			DWORD bytesToWrite = 0;
+			while (1) {
+
+				daemonHandle = IPCStaticLibrary::createDefaultPipe(IPCDefines::miStdString("\\\\.\\pipe\\MOCIPCDaemon"));
+				if (INVALID_HANDLE_VALUE == daemonHandle) {
+					MOCIPC_DBGPRINT("IPC server daemon pipe create failed!");
+					break;
+				}
+				if (!ConnectNamedPipe(daemonHandle, NULL)) {
+					CloseHandle(daemonHandle);
+					MOCIPC_DBGPRINT("IPC server daemon pip already created, trying to close...");
+					continue;
+				}
+				IPCDefines::miStdString newConnectionName = IPCDefines::miStdString("\\\\.\\pipe\\MOCIPC") + IPCStaticLibrary::generateRandomStdString(8);
+				newConnectionHandle = IPCStaticLibrary::createDefaultPipe(newConnectionName);
+				bytesToWrite = static_cast<DWORD>(IPCStaticLibrary::getExactSizeofString(newConnectionName));
+				if (!WriteFile(daemonHandle, (void*)newConnectionName.c_str(), bytesToWrite, &bytesWritten, NULL)) {
+					MOCIPC_DBGPRINT("Failed to write to pipe. Error: %d", GetLastError());
+					CloseHandle(newConnectionHandle);
+					continue;
+				}
+				connMap.emplace(connMap.size(), std::make_pair(newConnectionHandle, newConnectionName));
+				DisconnectNamedPipe(daemonHandle);
+				CloseHandle(daemonHandle);
+
+			}
+
+		});
+
+	}
 
 private:
-	
-	
+	std::thread connMgr;
+	/* { id : { pipeHandle : pipeName } } */
+	std::map< uint32_t, std::pair<HANDLE, IPCDefines::miStdString> >connMap;
 };
 
 class IPCClient : public IPCUnit {
 public:
-	
+	IPCClient()
+	{
+		connMgr = std::thread([this] {
+			HANDLE hPipe = INVALID_HANDLE_VALUE;
+			do {
+				if(!WaitNamedPipe("\\\\.\\pipe\\MOCIPCDaemon", NMPWAIT_WAIT_FOREVER))
+					continue;
+				hPipe = CreateFile(
+					"\\\\.\\pipe\\MOCIPCDaemon",
+					GENERIC_READ | GENERIC_WRITE,
+					FILE_SHARE_READ,
+					NULL,
+					OPEN_EXISTING,
+					0,
+					NULL
+				);
 
+
+			} while(1);
+			
+		});
+	}
 private:
-
+	std::thread connMgr;
 };
 
 } /* MocIPC */
