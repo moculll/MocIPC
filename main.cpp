f6c338d09bb3e09c2b5d95ed247ea36b11411fb3
mocul <823309460@qq.com>
2024-09-11 20:34:35 +0800

    still updating...

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "mocIPC.h"
+int main(int argc, char* argv[])
+{
+    MocIPC::IPCServer *server = new MocIPC::IPCServer();
+
+
+}
--- mocIPC.h
+++ mocIPC.h
@@ -5,8 +5,8 @@
 #include <vector>
 #include <string>
 #include <random>
-
-
+#include <functional>
+#include <future>
 #define MOCIPC_DBGPRINT_ENABLE 1
 #if MOCIPC_DBGPRINT_ENABLE
 
@@ -87,11 +87,6 @@ IPCDefines::miStdString generateRandomStdString(int length) {
 	return result;
 }
 	
-static inline HANDLE createDefaultPipe(const IPCDefines::miStdString &pipeName)
-{
-	return IPCStaticLibrary::createPipe(pipeName, 2048, 2048);
-}
-
 static inline HANDLE createPipe(const IPCDefines::miStdString &pipeName, uint32_t outBufferSize, uint32_t inBufferSize)
 {
 	return CreateNamedPipe(
@@ -106,28 +101,33 @@ static inline HANDLE createPipe(const IPCDefines::miStdString &pipeName, uint32_
 	);
 }
 
+static inline HANDLE createDefaultPipe(const IPCDefines::miStdString& pipeName)
+{
+	return IPCStaticLibrary::createPipe(pipeName, 2048, 2048);
+}
+
 } /* IPCStaticLibrary */
 
 class IPCUnit {
 public:
+
 	IPCUnit()
 	{
 		
 	}
 
-	HANDLE openChannel(const IPCDefines::miStdString &pipeName)
-	{
-		IPCStaticLibrary::createDefaultPipe(pipeName);
-		return ;
-	}
-	void closeChannel(HANDLE handleToClose)
-	{
-
-	}
+	
+	
 protected:
 	/*std::map<HANDLE, CHARTYPE*> a;*/
+	using recvHookType_t = std::function<void(void*)>;
+	struct serverInfo_t {
+		std::pair<HANDLE, IPCDefines::miStdString> connInfo;
+		std::atomic<bool> ready;
 
-
+		serverInfo_t(std::pair<HANDLE, IPCDefines::miStdString> &info, bool&& isReady)
+			: connInfo(info), ready(std::move(isReady)) {}
+	};
 
 private:
 	
@@ -142,8 +142,8 @@ class IPCServer : public IPCUnit {
 public:
 	
 	
-
-	IPCServer() 
+	
+	IPCServer()
 	{
 		connMgr = std::thread([this] {
 			HANDLE daemonHandle = INVALID_HANDLE_VALUE;
@@ -157,6 +157,7 @@ public:
 					MOCIPC_DBGPRINT("IPC server daemon pipe create failed!");
 					break;
 				}
+				/* block function */
 				if (!ConnectNamedPipe(daemonHandle, NULL)) {
 					CloseHandle(daemonHandle);
 					MOCIPC_DBGPRINT("IPC server daemon pip already created, trying to close...");
@@ -168,9 +169,22 @@ public:
 				if (!WriteFile(daemonHandle, (void*)newConnectionName.c_str(), bytesToWrite, &bytesWritten, NULL)) {
 					MOCIPC_DBGPRINT("Failed to write to pipe. Error: %d", GetLastError());
 					CloseHandle(newConnectionHandle);
+					DisconnectNamedPipe(daemonHandle);
+					CloseHandle(daemonHandle);
 					continue;
 				}
-				connMap.emplace(connMap.size(), std::make_pair(newConnectionHandle, newConnectionName));
+				std::atomic<bool> ready(false);
+				std::async(std::launch::async, [this, &ready, newConnectionHandle] {
+					ConnectNamedPipe(newConnectionHandle, NULL);
+					ready.store(true, std::memory_order_relaxed);
+
+					handleClientConnection(newConnectionHandle);
+				});
+				std::pair<HANDLE, IPCDefines::miStdString> connInfo = std::make_pair(newConnectionHandle, newConnectionName);
+				serverInfo_t serverInfo(connInfo, std::move(ready));
+
+				info.emplace(info.size(), std::move(serverInfo));
+
 				DisconnectNamedPipe(daemonHandle);
 				CloseHandle(daemonHandle);
 
@@ -179,24 +193,48 @@ public:
 		});
 
 	}
-
+	void registerRecvHOOK(recvHookType_t callback) {
+		recvHook = callback;
+	}
 private:
 	std::thread connMgr;
-	/* { id : { pipeHandle : pipeName } } */
-	std::map< uint32_t, std::pair<HANDLE, IPCDefines::miStdString> >connMap;
+	/* { id : info } */
+	static std::map<uint32_t, serverInfo_t> info;
+
+	recvHookType_t recvHook;
+	void handleClientConnection(HANDLE newConnectionHandle) {
+		char buffer[4096];
+		DWORD bytesRead;
+		while (ReadFile(newConnectionHandle, buffer, sizeof(buffer), &bytesRead, NULL)) {
+			if (recvHook)
+				recvHook(buffer);
+			
+		}
+	}
+	uint32_t write(int index, void* src, uint32_t size) {
+		DWORD bytesWritten = 0;
+		if(index > info.size() - 1)
+			return bytesWritten;
+		WriteFile(info[index].connInfo.first, src, size, &bytesWritten, NULL);
+		return bytesWritten;
+	}
 };
 
 class IPCClient : public IPCUnit {
 public:
-	IPCClient()
-	{
-		connMgr = std::thread([this] {
-			HANDLE hPipe = INVALID_HANDLE_VALUE;
-			do {
-				if(!WaitNamedPipe("\\\\.\\pipe\\MOCIPCDaemon", NMPWAIT_WAIT_FOREVER))
+	using MessageCallback = std::function<void(const IPCDefines::miStdString&)>;
+	using recvHookType_t = std::function<void(void*)>;
+
+	IPCClient(const IPCDefines::miStdString& publicPipeName) : publicPipeName(publicPipeName) {
+		 std::thread([this, publicPipeName] {
+			while (true) {
+				if (!WaitNamedPipe(publicPipeName.c_str(), NMPWAIT_WAIT_FOREVER)) {
+					MOCIPC_DBGPRINT("Failed to wait for named pipe");
 					continue;
-				hPipe = CreateFile(
-					"\\\\.\\pipe\\MOCIPCDaemon",
+				}
+
+				HANDLE hPipe = CreateFile(
+					publicPipeName.c_str(),
 					GENERIC_READ | GENERIC_WRITE,
 					FILE_SHARE_READ,
 					NULL,
@@ -204,14 +242,77 @@ public:
 					0,
 					NULL
 				);
+				if (hPipe == INVALID_HANDLE_VALUE) {
+					MOCIPC_DBGPRINT("Failed to connect to the daemon pipe. Error: %d", GetLastError());
+					continue;
+				}
 
+				char buffer[4096];
+				DWORD bytesRead;
+				while (ReadFile(hPipe, buffer, sizeof(buffer), &bytesRead, NULL)) {
+					IPCDefines::miStdString newConnectionName(buffer, bytesRead);
 
-			} while(1);
-			
-		});
+					HANDLE clientPipe = CreateFile(
+						newConnectionName.c_str(),
+						GENERIC_READ | GENERIC_WRITE,
+						FILE_SHARE_READ,
+						NULL,
+						OPEN_EXISTING,
+						0,
+						NULL
+					);
+					if (clientPipe != INVALID_HANDLE_VALUE) {
+						std::lock_guard<std::mutex> lock(mutex);
+						connections.emplace(clientPipe, newConnectionName);
+						handleServerConnection(clientPipe);
+						break;
+					}
+					else {
+						MOCIPC_DBGPRINT("Failed to connect to client pipe. Error: %d", GetLastError());
+					}
+				}
+				CloseHandle(hPipe);
+			}
+			});
 	}
+
+	void registerRecvHOOK(recvHookType_t callback) {
+		recvHook = callback;
+	}
+
+	void sendMessage(const IPCDefines::miStdString& message, HANDLE pipeHandle) {
+		if (pipeHandle != INVALID_HANDLE_VALUE) {
+			DWORD bytesWritten;
+			WriteFile(pipeHandle, message.c_str(), static_cast<DWORD>(message.size()), &bytesWritten, NULL);
+		}
+	}
+
+	uint32_t write(HANDLE pipeHandle, void* src, uint32_t size) {
+		DWORD bytesWritten = 0;
+		if (pipeHandle != INVALID_HANDLE_VALUE) {
+			WriteFile(pipeHandle, src, size, &bytesWritten, NULL);
+		}
+		return bytesWritten;
+	}
+
 private:
-	std::thread connMgr;
+	IPCDefines::miStdString publicPipeName;
+	std::map<HANDLE, IPCDefines::miStdString> connections;
+	recvHookType_t recvHook;
+	std::mutex mutex;
+
+	void handleServerConnection(HANDLE clientPipe) {
+		char buffer[4096];
+		DWORD bytesRead;
+		while (ReadFile(clientPipe, buffer, sizeof(buffer), &bytesRead, NULL)) {
+			if (recvHook) {
+				recvHook(buffer);
+			}
+		}
+		std::lock_guard<std::mutex> lock(mutex);
+		connections.erase(clientPipe);
+		CloseHandle(clientPipe);
+	}
 };
 
 } /* MocIPC */
